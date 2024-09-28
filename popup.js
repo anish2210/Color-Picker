e95b400aa09a4a35d54e7388e122b21cb0758bbd
anish2210 <anishjaiswal1220@gmail.com>
2024-09-28 13:52:24 +0530

    first commit during dev

---
+++ popup.js
@@ -0,0 +1,28 @@
+const btn = document.querySelector(".changeColorBtn");
+
+btn.addEventListener("click", async () => {
+  let [tab] = await chrome.tabs.query({ active: true, currentWindow: true });
+
+  chrome.scripting.executeScript({
+    target: { tabId: tab.id },
+    function: pickColor,
+  });
+});
+
+async function pickColor() {
+  try {
+    const eyeDropper = new EyeDropper();
+    const selectedColor = await eyeDropper.open();
+    console.log(selectedColor);
+  } catch (error) {
+    console.log(error);
+  }
+
+  // try{
+  //     const eyeDropper = new EyeDropper();
+  //     const selectedColor = await eyeDropper.open();
+  //     console.log(selectedColor);
+  // } catch (err) {
+  //     console.error(err);
+  // }
+}
